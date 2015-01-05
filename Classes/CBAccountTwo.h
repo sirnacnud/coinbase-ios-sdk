b45b6795b4c5612b937ca88474feaaba79ed375a
sir_nacnud <duncanc4@gmail.com>
2015-01-04 22:34:50 -0600

    Added getBalance to CBAccountTwo

--- Classes/CBAccountTwo.h
+++ Classes/CBAccountTwo.h
@@ -8,11 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+#include "CBHandlers.h"
+
 @interface CBAccountTwo : NSObject
 
 @property (nonatomic, strong) NSString *name;
 @property (nonatomic, strong) NSString *id;
 @property (nonatomic, strong) NSString *type;
 @property (nonatomic) BOOL primary;
+
+- (void)getBalance:(BalanceHandler)handler;
 
 @end
--- Classes/CBAccountTwo.m
+++ Classes/CBAccountTwo.m
@@ -7,7 +7,28 @@
 //
 
 #import "CBAccountTwo.h"
+#import "CBRequest.h"
+#include "CBTokens.h"
 
 @implementation CBAccountTwo
+
+- (void)getBalance:(BalanceHandler)handler {
+    [CBRequest authorizedRequest:^(NSDictionary *result, NSError *error) {
+        if (error) {
+            handler(nil, error);
+        } else {
+            AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+            manager.requestSerializer = [AFJSONRequestSerializer serializer];
+            manager.responseSerializer = [AFJSONResponseSerializer serializer];
+            [manager GET:[NSString stringWithFormat:@"https://api.coinbase.com/v1/accounts/%@/balance?access_token=%@", self.id, [CBTokens accessToken]] parameters:nil success:^(AFHTTPRequestOperation *operation, id JSON) {
+                
+                handler([JSON objectForKey:@"amount"], nil);
+                
+            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                handler(nil, error);
+            }];
+        }
+    }];
+}
 
 @end
