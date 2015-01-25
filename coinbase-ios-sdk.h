656f4fe3d0f2bdc6234bd16eabda3a201e82e07d
sir_nacnud <duncanc4@gmail.com>
2015-01-25 14:19:09 -0600

    Add files needed to be built as a framework

---
+++ coinbase-ios-sdk.h
@@ -0,0 +1,19 @@
+//
+//  coinbase-ios-sdk.h
+//  coinbase-ios-sdk
+//
+//  Created by Duncan Cunningham on 1/25/15.
+//  Copyright (c) 2015 Duncan Cunningham. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for coinbase-ios-sdk.
+FOUNDATION_EXPORT double coinbase_ios_sdkVersionNumber;
+
+//! Project version string for coinbase-ios-sdk.
+FOUNDATION_EXPORT const unsigned char coinbase_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <coinbase_ios_sdk/PublicHeader.h>
+
+
